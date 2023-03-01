af4ec0d3d6efa6d5a2bf0f70c0fbc3a4733e7382
Alvarojavier22 <alvarojavierchagas@hotmail.com>
2023-03-01 20:01:51 -0300

    La barra sigue oculta

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,33 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', 'v', 'a', 'r', 'o']
+    const jobArray = ['F', 'u', 'l', 'l', ' ', 'S', 't', 'a', 'c', 'k', ' ', 'S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm 
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass}_12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass}_13`}>I</span>
+                    <span className={`${letterClass}_14`}>'m,</span>
                  <img src={LogoTitle} alt="developer" />
-                lvaro
+                    <AnimatedLetters letterClass={letterClass} 
+                        strArray={nameArray}
+                        idx={15}
+                />
                 <br />
-                Full Stack Software Developer
+                <AnimatedLetters letterClass={letterClass} 
+                        strArray={jobArray}
+                        idx={22}
+                />
                 </h1>
                 <h2>Frontend Developer / Javascript expert</h2>
                 <h2>Backend Developer / Python expert</h2>
